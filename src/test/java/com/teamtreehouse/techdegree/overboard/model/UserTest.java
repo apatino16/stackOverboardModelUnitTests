063f904eee3273e2da54b0fc757433c1a3e97f49
Anny Patino <103137175+apatino16@users.noreply.github.com>
2024-11-20 16:21:21 -0800

    Set-up UserTest correctly

---
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.techdegree.overboard.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
