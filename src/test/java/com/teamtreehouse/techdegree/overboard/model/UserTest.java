98a6d4da71d473e74b62f637041d6d73ef23fd87
Anny Patino <103137175+apatino16@users.noreply.github.com>
2024-11-20 16:52:26 -0800

    Test for Reputation Change

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -1,13 +1,36 @@
 package com.teamtreehouse.techdegree.overboard.model;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+// Test Fixture for the User model
 public class UserTest {
-
+    private Board board;
+    private User user;
+    private Question question;
+    private Answer answer;
 
     @Before
     public void setUp() throws Exception {
     }
+
+    // Test for Reputation Changes
+    @Test
+    public void questionerReputationIncreasesBy5WhenQuestionIsUpVoted() {
+
+    }
+
+    @Test
+    public void questionerReputationIncreasesBy10WhenAnswerIsUpVoted() {
+
+    }
+
+    @Test
+    public void acceptedAnswerGives15ReputationBoost() {
+
+    }
+
+
 }
