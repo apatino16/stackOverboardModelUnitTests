8692e6a58a84b3e9f779fa6565446df7829b28d1
Anny Patino <103137175+apatino16@users.noreply.github.com>
2024-11-20 23:01:24 -0800

    Commented code, implement comprehensive reputation tests
    Added additional reputation tests for when a question or answer gets down voted

--- src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
+++ src/test/java/com/teamtreehouse/techdegree/overboard/model/UserTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.assertTrue;
 
 // Test Fixture for the User model
 public class UserTest {
-    // Initialized Users
+    // Shared test objects
     private User answerer;
     private User questioner;
     private User voter;
@@ -42,39 +42,64 @@ public class UserTest {
     // Validates Questioner's reputation increases by 5 points if  their question is up-voted
     @Test
     public void questionerReputationIncreasesBy5WhenQuestionIsUpVoted() throws Exception {
-        // Act
+        // Act: Voter upVotes the question
         voter.upVote(question);
 
-        // Assert
+        // Assert: Questioner's reputation increases by 5
         assertEquals(5, questioner.getReputation());
     }
 
     // Validate Answerer's reputation increases by 10 points if their answer is up-voted
     @Test
     public void answererReputationIncreasesBy10WhenAnswerIsUpVoted() throws Exception {
-        // Act
+        // Act: Voter upVotes the answer
         voter.upVote(answer);
 
-        // Assert
+        // Assert: Answerer's reputation increases by 10
         assertEquals(10, answerer.getReputation());
     }
 
     // Validates Answerer's reputation increases by 15 points if their answer is accepted
     @Test
     public void answererReputationIncreasesBy15IfAnswerIsAccepted() throws Exception {
-        // Act
+        // Act: Questioner accepts the answer
         questioner.acceptAnswer(answer);
 
-        // Assert
+        // Assert: Answerer's reputation increases by 15
         assertEquals(15, answerer.getReputation());
     }
 
-    // Test that Authors are prevented from self-voting
+    // ---------- Extra Credit Starts-------- //
+
+    // currently down-voting of questions affects nothing.
+    @Test
+    public void questionerReputationRemainTheSameWhenQuestionIsDownVoted() throws Exception {
+        // Act: Voter upVotes the question
+        voter.downVote(question);
+
+        // Assert: Questioner's reputation increases by 5
+        assertEquals(0, questioner.getReputation());
+    }
+
+    // Reputation deduction for downVoted answers costs 1 point
+    @Test
+    public void answererReputationDecreasesBy1WhenAnswerIsDownVoted() throws Exception {
+        // Act: Voter upVotes the answer
+        voter.downVote(answer);
+
+        // Assert: Answerer's reputation increases by 10
+        assertEquals(-1, answerer.getReputation());
+    }
+    
+    // ---------- Extra Credit Ends ------- //
+
+
+    // Test for Self-voting Restrictions
 
     // Verify that users cannot upVote their own question
     @Test
     public void userUpVotesTheirOwnQuestion() throws Exception {
-
+        // Act and Assert: Expect a VotingException with appropriate message
         thrown.expect(VotingException.class);
         thrown.expectMessage("You cannot vote for yourself!");
 
@@ -84,7 +109,7 @@ public class UserTest {
     // Verify that user cannot downVote their own question
     @Test
     public void userDownVotesTheirOwnQuestion() throws Exception {
-
+        // Act and Assert: Expect a VotingException with appropriate message
         thrown.expect(VotingException.class);
         thrown.expectMessage("You cannot vote for yourself!");
 
@@ -94,6 +119,7 @@ public class UserTest {
     // Verify that user cannot upVote their own answer
     @Test
     public void userUpVotesTheirOwnAnswer() throws Exception {
+        // Act and Assert: Expect a VotingException with appropriate message
         thrown.expect(VotingException.class);
         thrown.expectMessage("You cannot vote for yourself!");
 
@@ -102,9 +128,9 @@ public class UserTest {
     }
 
     // Verify that user cannot downVote their own answer
-
     @Test
     public void userDownVotesTheirOwnAnswer() throws Exception {
+        // Act and Assert: Expect a VotingException with appropriate message
         thrown.expect(VotingException.class);
         thrown.expectMessage("You cannot vote for yourself!");
 
@@ -112,12 +138,12 @@ public class UserTest {
 
     }
 
-    // Only the original questioner can accept an answer
+    // Test for Answer Acceptance
 
     // Non-author user cannot accept an answer to a question
     @Test
     public void nonAuthorAcceptsAnswerToQuestion() throws Exception {
-        // Act and Assert
+        // Act and Assert: Expect a AnswerAcceptanceException with appropriate message
         thrown.expect(AnswerAcceptanceException.class);
         thrown.expectMessage("Only Questioner can accept this answer as it is their question");
 
@@ -127,10 +153,10 @@ public class UserTest {
     // Original questioner is able to accept an answer
     @Test
     public void originalQuestionerAcceptsAnswer() throws Exception {
-        // Act
+        // Act: Questioner accepts the answer
         questioner.acceptAnswer(answer);
 
-        // Assert
+        // Assert: Answer is marked as accepted
         assertTrue(answer.isAccepted());
     }
 
